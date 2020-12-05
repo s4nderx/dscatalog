b67baab6f7b65f847dd195d4f1b82a9faa5fe222
Phellipe Sander <phellipe.sander1@gmail.com>
2020-12-05 20:10:38 -0300

    product card component

--- front-web/src/pages/catalog/index.tsx
+++ front-web/src/pages/catalog/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import ProductCard from './components/ProductCard';
 import './styles.scss'
 const Catalog = () => (
     <div className="catalog-container">
         <h1 className="catalog-title">Catálogo de produtos</h1>
         <div className="catalog-products">
-            <h1>product1</h1>
+            <ProductCard />
+            <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
         </div>
     </div>
 );
