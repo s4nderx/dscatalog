3ae83f83afd128ef3ad002798bbf192dea2d2df5
Phellipe <phellipe.sander1@gmail.com>
2020-12-21 18:55:31 -0300

    create one pruduct

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,3 +1,4 @@
+import { http_request } from 'core/utils/request';
 import BaseForm from 'pages/Admin/components/BaseForm';
 import { useState } from 'react';
 
@@ -5,65 +6,88 @@ type FormState = {
     name: string;
     price: string;
     category: string;
+    description: string;
 }
 
+type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
 const Form = () => {
 
     const [formData, setFormData] = useState<FormState>({
         name: '',
         price: '',
-        category: ''
+        category: '',
+        description: '',
     });
 
-    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const handleOnChange = (event: FormEvent) => {
         const name = event.target.name;
         const value = event.target.value;
-        console.log({name, value});
-        
         setFormData(data => ({ ...data, [name]: value}));
     };
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(formData);
+        const payLoad = {
+            ...formData,
+            imgUrl:
+                'https://compass-ssl.xbox.com/assets/83/53/83534a33-0998-43dc-915a-4ec0a686d679.jpg?n=10202018_Panes-3-up-1400_Hero-SX_570x570.jpg',
+            cateries: [{ id: formData.category }]
+        };
+
+        http_request({ url: '/products', method: 'POST', data: payLoad }).then(() => {
+                setFormData({ name: '', category: '', price: '', description: ''});
+            }
+        );
         
     }
 
     return (
         <form onSubmit={handleSubmit}>
-                <BaseForm title="CADASTRAR UM PRODUTO">
-                    <div className="row">
-                        <div className="col-6">
-                            <input
-                                value={formData.name}
-                                name="name"
-                                type="text"
-                                className="form-control mb-5"
-                                onChange={handleOnChange}
-                                placeholder="Nome do Produto"
-                            />
+            <BaseForm title="CADASTRAR UM PRODUTO">
+                <div className="row mt-5">
+                    <div className="col-6">
+                        <input
+                            value={formData.name}
+                            name="name"
+                            type="text"
+                            className="form-control mb-5"
+                            onChange={handleOnChange}
+                            placeholder="Nome do Produto"
+                        />
 
-                            <select
-                                value={formData.category}
-                                className="form-control mb-5"
-                                onChange={handleOnChange}
-                                name="category"
-                            >
-                                <option value="livros">Livros</option>
-                                <option value="computadores">Computadores</option>
-                                <option value="eletronicos">Eletrônicos</option>
-                            </select>
+                        <select
+                            value={formData.category}
+                            className="form-control mb-5"
+                            onChange={handleOnChange}
+                            name="category"
+                        >
+                            <option value="1">Livros</option>
+                            <option value="3">Computadores</option>
+                            <option value="2">Eletrônicos</option>
+                        </select>
 
-                            <input
-                                value={formData.price}
-                                name="price"
-                                type="text"
-                                className="form-control"
-                                onChange={handleOnChange}
-                            />
-                        </div>
+                        <input
+                            value={formData.price}
+                            name="price"
+                            type="text"
+                            className="form-control"
+                            onChange={handleOnChange}
+                        />
                     </div>
-                </BaseForm>         
+                    <div className="col-6">
+                        <textarea
+                        name="description"
+                        onChange={handleOnChange}
+                        id="" 
+                        className="form-control"
+                        cols={30} 
+                        rows={10}
+                        value={formData.description}>
+
+                        </textarea>
+                    </div>
+                </div>
+            </BaseForm>
         </form>
     );
 };
--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -1,37 +1,21 @@
-import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import List from './List';
 import Form from './Form';
 const Products = () => {
     return (
-        <div>
-            <Link className="mr-5" to="/admin/products">
-                
-                Listar produtos
-            
-            </Link>
-            <Link className="mr-5" to="/admin/products/create">
-                
-                Criar produto
-            
-            </Link>
-            <Link className="mr-5" to="/admin/products/10">
-                
-                Editar produto
-            
-            </Link>
-            <Switch>
-                <Route path="/admin/products" exact>
-                    <List />
-                </Route>
-                <Route path="/admin/products/create">
-                    <Form  />
-                </Route>
-                <Route path="/admin/products/:productId">
-                    <h1>editar um produto</h1>
-                </Route>
-            </Switch>
-        </div>
+      <div>
+        <Switch>
+          <Route path="/admin/products" exact>
+            <List />
+          </Route>
+          <Route path="/admin/products/create">
+            <Form />
+          </Route>
+          <Route path="/admin/products/:productId">
+            <h1>editar um produto</h1>
+          </Route>
+        </Switch>
+      </div>
     );
 };
 
