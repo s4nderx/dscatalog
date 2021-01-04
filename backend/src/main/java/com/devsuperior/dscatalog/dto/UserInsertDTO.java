036e6adec40c29d81bf3394c7ea767e4809ae512
s4nderx <phellipe.sander1@gmail.com>
2021-01-03 21:04:40 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.dto;
+
+public class UserInsertDTO extends UserDTO {
+    private static final long serialVersionUID = 5099868662655233602L;
+
+    private String password;
+
+    public UserInsertDTO(String password) {
+        super();
+        this.password = password;
+    }
+
+    public UserInsertDTO() {
+        super();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
